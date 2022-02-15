751a05d7c1dbcfbb9a6ba3a4fa6fa9d327c3fe99
Riad Younes <riad.younes@hotmail.com>
2022-02-14 22:09:05 -0300

    finish challenge

--- src/util/format.ts
+++ src/util/format.ts
@@ -2,3 +2,7 @@ export const { format: formatPrice } = new Intl.NumberFormat('pt-br', {
   style: 'currency',
   currency: 'BRL',
 });
+
+export const formatDate = new Intl.DateTimeFormat('pt-br', {
+  dateStyle: 'short'
+}).format();
